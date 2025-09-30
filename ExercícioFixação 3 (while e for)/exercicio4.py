d48df3cfef6088559a52285ebc465c49513eb370
brendolalves <brendol.alves@gmail.com>
2025-09-30 16:29:22 -0300

    Update exercicio4.py

--- ExercícioFixação 3 (while e for)/exercicio4.py
+++ ExercícioFixação 3 (while e for)/exercicio4.py
@@ -6,11 +6,26 @@ as notas necessárias (100, 50, 20, 10, 5,
 """
 
 cash = int(input("Qual o valor da retirada?: "))
-x= 0
-peixe = 0
+cem = 0
 
-while x <=cash:
-    x = x*100 + peixe
-    print(x)
-
-    
+while cem*100 <=cash:
+    print(cem, " notas de 100")
+    cem=cem+1
+r1= cash-((cem-1)*100)
+print(r1)
+ciq=0
+while ciq*50<=r1:
+    print(ciq, " notas de 50")
+    ciq=ciq+1
+r2 = r1-((ciq-1)*50)
+print(r2)
+vin=0
+while vin*20<=r2:
+    print(vin, " notas de 20")
+    vin=vin+1
+r3=r2-((vin-1)*20)
+print(r3)
+dez=0
+while dez<=r3:
+    print(dez, " notas de 10")
+    dez=dez+1
